20dca47f50a07806ba21c27091f6ae313c727f1c
Henry.Liao <Henry.Liao@skylinetw.com>
2024-05-30 16:34:55 +0800

    feat: implement user login

--- src/main/java/com/henry/model/UserMapper.java
+++ src/main/java/com/henry/model/UserMapper.java
@@ -1,5 +1,8 @@
 package com.henry.model;
 
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 public interface UserMapper {
     User selectUser(int id);
     int insertUser(User user);
@@ -7,4 +10,9 @@ public interface UserMapper {
     void updateUser(User user);
 
     void deleteUser(int id);
+
+    User login(String username, String password);
+
+    @Select("SELECT * FROM ssm.t_user WHERE username = #{username} AND password = #{password}")
+    User login2(@Param("username") String username, @Param("password") String password);
 }
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -12,6 +12,20 @@ public class UserTest {
         sql.close();
     }
     @Test
+    public void login() {
+        SqlUtil sql = new SqlUtil("mybatis-config.xml");
+        User user = sql.GetUserMapper().login("user1", "s12345");
+        System.out.println(user.toString());
+        sql.close();
+    }
+    @Test
+    public void login2() {
+        SqlUtil sql = new SqlUtil("mybatis-config.xml");
+        User user = sql.GetUserMapper().login("user1", "s12345");
+        System.out.println(user.toString());
+        sql.close();
+    }
+    @Test
     public void TestInsert() {
         SqlUtil sql = new SqlUtil("mybatis-config.xml");
         User userParam = new User();
