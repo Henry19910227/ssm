818ab87edd641ebd01a212b9c3e374599b0df695
Henry.Liao <Henry.Liao@skylinetw.com>
2024-06-14 13:06:34 +0800

    feat: Bean Scope

---
+++ ioc-bean_scope/src/test/java/UserTest.java
@@ -0,0 +1,27 @@
+import com.henry.model.User;
+import com.henry.model.User2;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserTest {
+    @Test
+    public void TestUser() {
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("spring-config.xml");
+        User user = ioc.getBean(User.class);
+        user.setNickname("Jeff");
+        User user2 = ioc.getBean(User.class);
+        System.out.println(user);
+        System.out.println(user2);
+    }
+
+    @Test
+    public void TestUser2() {
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("spring-config.xml");
+        User2 user = ioc.getBean(User2.class);
+        user.setNickname("Jeff");
+        User2 user2 = ioc.getBean(User2.class);
+        System.out.println(user);
+        System.out.println(user2);
+    }
+}
