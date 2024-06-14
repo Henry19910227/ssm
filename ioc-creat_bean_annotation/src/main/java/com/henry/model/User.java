030fe4eb18d615c8c09ae4a32eca44bd75a5cc5d
Henry.Liao <Henry.Liao@skylinetw.com>
2024-06-14 15:35:35 +0800

    feat: 使用 Annotation 創建 Bean

---
+++ ioc-creat_bean_annotation/src/main/java/com/henry/model/User.java
@@ -0,0 +1,56 @@
+package com.henry.model;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Controller
+public class User {
+    private Integer id;
+    private String email;
+    private String nickname;
+    private String createAt;
+    private String updateAt;
+
+    public User(Integer id, String email, String nickname, String createAt, String updateAt) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+    public User() {
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+
+    public void setUpdateAt(String updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", createAt='" + createAt + '\'' +
+                ", updateAt='" + updateAt + '\'' +
+                '}';
+    }
+}
