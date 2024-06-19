76cc23e7f1c551a27b5b58ca7e5cfa8eb5b1aa56
Henry.Liao <Henry.Liao@skylinetw.com>
2024-06-19 13:11:03 +0800

    feat: interceptor

--- springmvc/src/main/java/com/henry/config/Config.java
+++ springmvc/src/main/java/com/henry/config/Config.java
@@ -1,10 +1,12 @@
 package com.henry.config;
 
+import com.henry.interceptor.Interceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
@@ -28,5 +30,10 @@ public class Config implements WebMvcConfigurer {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new Interceptor()).addPathPatterns("/user/**").excludePathPatterns("/user/addUser");
     }
 }
